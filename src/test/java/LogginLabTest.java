98019ce7082192668b0451097f3e21e4f5fc2daa
ninachen22 <116048270+ninachen22@users.noreply.github.com>
2022-10-28 15:38:36 -0400

    made changes

--- src/test/java/LogginLabTest.java
+++ src/test/java/LogginLabTest.java
@@ -21,14 +21,36 @@ public class LogginLabTest {
         LogginLab lab = new LogginLab();
         lab.setThreshold(finalLimit);
 
-        for (Integer i = 1; i <= finalLimit; i++) {
+        for (Integer i = 1; i <= 8; i++) {
             if (lab.thresholdExceeds(i)) {
                 logger.log(Level.INFO, "Threshold not reached! It is "+i);
                 assertTrue(lab.thresholdExceeds(i));
+            } else if (lab.thresholdReached(i)) {
+                logger.log(Level.INFO, "Threshold passed!");
+                assertTrue(lab.thresholdReached(i));
+                break;
             } else {
                 logger.log(Level.INFO, "Threshold finally reached!");
-                assertFalse(lab.thresholdExceeds(i));
+                assertFalse(lab.thresholdExceeds(finalLimit)); //test still runs with integer i but finalLimit makes sense bc should only return for threshold which is final limit
             }
         }
     }
+
+//    @org.junit.Test
+//    public void thresholdReached() {
+//        Integer finalLimit = 5;
+//
+//        LogginLab lab = new LogginLab();
+//        lab.setThreshold(finalLimit - 1);
+//
+//        for (Integer i =1; i < 8; i++) {
+//            if (lab.thresholdReached(i)) {
+//                logger.log(Level.INFO, "Threshold reached! It is "+i);
+//                assertTrue(lab.thresholdReached(i));
+//            } else {
+//                logger.log(Level.INFO, "Threshold not reached!");
+//                assertFalse(lab.thresholdReached(i));
+//            }
+//        }
+//    }
 }
